b6827982b2c8d23a3b6b851a035fcc423fe09e2c
Gur Ronen <Gur.Ronen@Outlook.com>
2017-06-19 22:14:34 +0300

    work on createForm

--- src/FetchClass.js
+++ src/FetchClass.js
@@ -17,5 +17,23 @@ class FetchClass {
             console.error(err);
         });
     }
+
+    submitForms(formId, model, userId, callback) {
+        fetch('http://1.1.1.18/5000/forms/active/$userId'.replace('$userId', userId), {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: {
+                id: formId,
+                data: model
+            }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            callback(data)
+        });
+    }
 }
 export default FetchClass;
